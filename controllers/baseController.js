36b3e3b491f1f0de6391694cedafd48e1fc07a4c
wadeharris001 <wadeharris001@gmail.com>
2024-11-24 22:28:37 +0200

    week 3 complete

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,13 +1,13 @@
-const utilities = require("../utilities");
-const baseController = {};
+const utilities = require("../utilities/")
+const baseController = {}
 
-baseController.buildHome = async (req, res) => {
-    const nav = await utilities.getNav();
-    res.render("index", { title: "Home", nav, pagecss: "home" });
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
 }
 
 baseController.InternalServerError = async (req, res) => {
-    res.render("index", { title: "Home", nav, pagecss: "home" });
+    res.render("index", { title: "Home", nav, pagecss: "home" })
 }
 
 module.exports = baseController;
--- controllers/invController.js
+++ controllers/invController.js
@@ -1,38 +1,35 @@
-const invModel = require("../models/inventory-model");
-const utilities = require("../utilities");
-const invcss = "inv";
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
-const invCont = {};
+const invCont = {}
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
-invCont.buildByClassificationId = async (req, res, next) => {
-    const classification_id = req.params.classificationId;
-    const data = await invModel.getInventoryByClassificationId(classification_id);
-    const grid = await utilities.buildClassificationGrid(data);
-    let nav = await utilities.getNav();
-    const className = data[0].classification_name;
-    res.render("./inventory/classification", {
-        title: className + " vehicles",
-        nav,
-        grid,
-        pagecss: invcss
-    });
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
 }
 
 invCont.buildByInvId = async (req, res, next) => {
-    const inv_id = req.params.invId;
-    const data = await invModel.getVehicleByInvId(inv_id);
-    const details = await utilities.buildVehicleDetails(data);
-    let nav = await utilities.getNav();
-    const vehicleName = data.inv_make + ' ' + data.inv_model;
+    const inv_id = req.params.invId
+    const data = await invModel.getVehicleByInvId(inv_id)
+    const details = await utilities.buildVehicleDetails(data)
+    let nav = await utilities.getNav()
+    const vehicleName = data.inv_make + ' ' + data.inv_model
     res.render("./inventory/details", {
         title: vehicleName + " Details",
         nav,
         details,
-        pagecss: invcss
-    });
+    })
 }
 
 module.exports = invCont;
