95dc5fa73154cd9ffe898e303c8186853d1726fd
wadeharris001 <wadeharris001@gmail.com>
2024-11-24 22:19:22 +0200

    week 3

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,7 +5,9 @@ const utilities = require("../utilities")
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
+
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 router.get("/detail/:invId", utilities.handleErrors(invController.buildByInvId))
+
 
 module.exports = router;
