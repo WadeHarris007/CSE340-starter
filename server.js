fe24217589b02002b1d000807585390404ab9fd7
wadeharris001 <wadeharris001@gmail.com>
2024-12-02 23:47:34 +0200

    Week 4 update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -12,6 +14,44 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities")
+const bodyParser = require("body-parser")
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+/**
+ * EJS Configuration
+ */
+//env.config();
+app.use(require("./routes/static"));
+app.set("view engine", "ejs");
+app.use(require("express-ejs-layouts"));
+app.set("layout", "./layouts/layout");
+/**
+
 
 
 /* ***********************
@@ -32,6 +72,9 @@ app.get("/error", utilities.handleErrors(baseController.InternalServerError))
 
 // Inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
+
+// Account Routes
+app.use("/account", require("./routes/accountRoute"))
 
 // File Not Found Route
 app.use(async (req, res, next) => {
